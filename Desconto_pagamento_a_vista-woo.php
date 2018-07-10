cd0e86c1b8464a9e9a75bf1033b0bcd3089f7e0a
EVCODE <41021903+eduvll@users.noreply.github.com>
2018-07-09 22:11:33 -0300

    Add files via upload

---
+++ Desconto_pagamento_a_vista-woo.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Exibir Desconto para Pagamento à vista - Woocommerce
+ * Plugin URI: http://www.evcode.com.br
+ * Description: Display price in single product page with discount for cash payment.
+ * Author: EVCODE
+ * Author URI: http://evcode.com.br
+ * Version: 1.0
+ */
+
+function price_discount () {
+	global $product;
+	$currency = get_woocommerce_currency_symbol();	
+	$desc = 10 / 100;
+	$price = $product->get_price_including_tax();
+	$price_disc = $price * $desc;
+	$final_price = $price - $price_disc;
+	echo '<div class="price_disc_cash"><p>ou '.$currency.' '.$final_price.' para pagamento à vista</p></div>';
+}
+
+add_action ('ocean_after_single_product_price', 'price_discount');
